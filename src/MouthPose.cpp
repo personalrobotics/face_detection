19a63f25cacb2a3a541a8f01d003eb065d8582f4
AshwinSrinivas1994 <ashrulez24x7@gmail.com>
2018-10-26 17:00:13 -0700

    fixed apparent rotation after adding rotation to visualization

--- src/MouthPose.cpp
+++ src/MouthPose.cpp
@@ -79,16 +79,30 @@ std::vector<cv::Point3d> get3dModelPoints() {
   // Stomion Origin
   // X direction points forward projecting out of the person's stomion
 
-  modelPoints.push_back(cv::Point3d(0., 0., 0.));          // Stommion
-  modelPoints.push_back(cv::Point3d(-30.0, -65.5, 70.0));  // Right Eye
-  modelPoints.push_back(cv::Point3d(-30.0, 65.5, 70.));    // Left Eye
-  modelPoints.push_back(cv::Point3d(11.0, 0., 27.0));      // Nose
+//  modelPoints.push_back(cv::Point3d(0., 0., 0.));          // Stommion
+//  modelPoints.push_back(cv::Point3d(-30.0, -65.5, 70.0));  // Right Eye
+//  modelPoints.push_back(cv::Point3d(-30.0, 65.5, 70.));    // Left Eye
+//  modelPoints.push_back(cv::Point3d(11.0, 0., 27.0));      // Nose
+//  modelPoints.push_back(cv::Point3d(-10.0, 0.0, 75.0));    // Sellion
+//  modelPoints.push_back(cv::Point3d(-10.0, 0., -58.0));    // Menton
+//  modelPoints.push_back(cv::Point3d(-10.0, -3.4, 75.0));   // Right Eye Lid
+//  modelPoints.push_back(cv::Point3d(-10.0, 3.4, 75.0));    // Left Eye Lid
+//  modelPoints.push_back(cv::Point3d(-5.0, -2.5, 0.0));     // Right Lip corner
+//  modelPoints.push_back(cv::Point3d(-5.0, 2.5, 0.0));      // Left Lip corner
+//
+
+  modelPoints.push_back(cv::Point3d(0., 0., 0.));  // Stommion
+  modelPoints.push_back(cv::Point3d(-30.0, -65.5,70.0));  // Right Eye
+  modelPoints.push_back(cv::Point3d(-30.0, 65.5,70.));   // Left Eye
+  modelPoints.push_back(cv::Point3d(11.0, 0., 27.0));  // Nose
   modelPoints.push_back(cv::Point3d(-10.0, 0.0, 75.0));    // Sellion
-  modelPoints.push_back(cv::Point3d(-10.0, 0., -58.0));    // Menton
-  modelPoints.push_back(cv::Point3d(-10.0, -3.4, 75.0));   // Right Eye Lid
-  modelPoints.push_back(cv::Point3d(-10.0, 3.4, 75.0));    // Left Eye Lid
-  modelPoints.push_back(cv::Point3d(-5.0, -2.5, 0.0));     // Right Lip corner
-  modelPoints.push_back(cv::Point3d(-5.0, 2.5, 0.0));      // Left Lip corner
+  modelPoints.push_back(cv::Point3d(-10.0, 0.,-58.0));    // Menton
+  modelPoints.push_back(cv::Point3d(-10.0,-3.4,75.0)); // Right Eye Lid
+  modelPoints.push_back(cv::Point3d(-10.0,3.4,75.0)); // Left Eye Lid
+  modelPoints.push_back(cv::Point3d(-5.0,-2.5,0.0)); // Right Lip corner
+  modelPoints.push_back(cv::Point3d(-5.0,2.5,0.0)); // Left Lip corner
+  modelPoints.push_back(cv::Point3d(-115.0,-77.5,69.0)); // Right side
+  modelPoints.push_back(cv::Point3d(-115.0,77.5,69.0));  // Left side
 
   return modelPoints;
 }
@@ -113,28 +127,43 @@ std::vector<cv::Point2d> get2dImagePoints(full_object_detection& d) {
     Menton  */
 
   // Stomion Origin
-  indexStommion = imagePoints.size();
-  imagePoints.push_back(
-      cv::Point2d((d.part(62).x() + d.part(66).x()) * 0.5,
-                  (d.part(62).y() + d.part(66).y()) * 0.5));  // Stommion
-  imagePoints.push_back(
-      cv::Point2d(d.part(36).x(), d.part(36).y()));  // Right Eye
-  imagePoints.push_back(
-      cv::Point2d(d.part(45).x(), d.part(45).y()));  // Left Eye
-  imagePoints.push_back(cv::Point2d(d.part(30).x(), d.part(30).y()));  // Nose
-  imagePoints.push_back(
-      cv::Point2d(d.part(27).x(), d.part(27).y()));                  // Sellion
-  imagePoints.push_back(cv::Point2d(d.part(8).x(), d.part(8).y()));  // Menton
-  indexRightEyeLid = imagePoints.size();
-  imagePoints.push_back(
-      cv::Point2d(d.part(38).x(), d.part(38).y()));  // Right Eye Lid
-  indexLeftEyeLid = imagePoints.size();
-  imagePoints.push_back(
-      cv::Point2d(d.part(43).x(), d.part(43).y()));  // Left Eye Lid
-  imagePoints.push_back(
-      cv::Point2d(d.part(48).x(), d.part(48).y()));  // Right Lip Corner
-  imagePoints.push_back(
-      cv::Point2d(d.part(54).x(), d.part(54).y()));  // Left Lip Corner
+//  indexStommion = imagePoints.size();
+//  imagePoints.push_back(
+//      cv::Point2d((d.part(62).x() + d.part(66).x()) * 0.5,
+//                  (d.part(62).y() + d.part(66).y()) * 0.5));  // Stommion
+//  imagePoints.push_back(
+//      cv::Point2d(d.part(36).x(), d.part(36).y()));  // Right Eye
+//  imagePoints.push_back(
+//      cv::Point2d(d.part(45).x(), d.part(45).y()));  // Left Eye
+//  imagePoints.push_back(cv::Point2d(d.part(30).x(), d.part(30).y()));  // Nose
+//  imagePoints.push_back(
+//      cv::Point2d(d.part(27).x(), d.part(27).y()));                  // Sellion
+//  imagePoints.push_back(cv::Point2d(d.part(8).x(), d.part(8).y()));  // Menton
+//  indexRightEyeLid = imagePoints.size();
+//  imagePoints.push_back(
+//      cv::Point2d(d.part(38).x(), d.part(38).y()));  // Right Eye Lid
+//  indexLeftEyeLid = imagePoints.size();
+//  imagePoints.push_back(
+//      cv::Point2d(d.part(43).x(), d.part(43).y()));  // Left Eye Lid
+//  imagePoints.push_back(
+//      cv::Point2d(d.part(48).x(), d.part(48).y()));  // Right Lip Corner
+//  imagePoints.push_back(
+//      cv::Point2d(d.part(54).x(), d.part(54).y()));  // Left Lip Corner
+
+      imagePoints.push_back( cv::Point2d( (d.part(62).x()+
+  d.part(66).x())*0.5, (d.part(62).y()+d.part(66).y())*0.5 ) );             // Stommion
+  //imagePoints.push_back( cv::Point2d( d.part(66).x(),d.part(66).y()  ) );             // Stommion
+  imagePoints.push_back( cv::Point2d( d.part(36).x(), d.part(36).y() ) );   // Right Eye
+  imagePoints.push_back( cv::Point2d( d.part(45).x(), d.part(45).y() ) );   // Left Eye
+  imagePoints.push_back( cv::Point2d( d.part(30).x(), d.part(30).y() ) );   // Nose
+  imagePoints.push_back( cv::Point2d( d.part(27).x(), d.part(27).y() ) );   // Sellion
+  imagePoints.push_back( cv::Point2d( d.part(8).x(), d.part(8).y() ) );     // Menton
+  imagePoints.push_back( cv::Point2d( d.part(38).x(), d.part(38).y() ) );     // Right Eye Lid
+  imagePoints.push_back( cv::Point2d( d.part(43).x(), d.part(43).y() ) );     // Left Eye Lid
+  imagePoints.push_back( cv::Point2d( d.part(48).x(), d.part(48).y() ) );     // Right Lip Corner
+  imagePoints.push_back( cv::Point2d( d.part(54).x(), d.part(54).y() ) );     // Left Lip Corner
+  imagePoints.push_back( cv::Point2d( d.part(0).x(), d.part(0).y() ) );     // Right Side
+  imagePoints.push_back( cv::Point2d( d.part(16).x(), d.part(16).y() ) );     // Left Side
 
   return imagePoints;
 }
@@ -205,16 +234,25 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
 
       cv::Mat R;
 
+
       cv::solvePnP(modelPoints, imagePoints, cameraMatrix, distCoeffs,
                    rotationVector, translationVector);
 
       Eigen::Vector3d Translate;
-      
+
 
       cv::Rodrigues(rotationVector, R);
 
+      Eigen::AngleAxisd rollAngle(3.14159, Eigen::Vector3d::UnitZ());
+      Eigen::AngleAxisd yawAngle(0, Eigen::Vector3d::UnitY());
+      Eigen::AngleAxisd pitchAngle(0, Eigen::Vector3d::UnitX());
+      Eigen::Quaternion<double> q = rollAngle * yawAngle * pitchAngle;
+      Eigen::Matrix3d zRot = q.matrix();
+
+
       Eigen::Matrix3d mat;
       cv::cv2eigen(R, mat);
+      mat=zRot*mat;
 
       Eigen::Quaterniond EigenQuat(mat);
 
@@ -278,10 +316,17 @@ void publishMarker(float tx, float ty, float tz) {
   new_marker.pose.position.y = ty;
   new_marker.pose.position.z = tz;
 
-  new_marker.pose.orientation.x = 0.707;
-  new_marker.pose.orientation.y = 0;
-  new_marker.pose.orientation.z = 0;
-  new_marker.pose.orientation.w = 0.707;
+  // Accounting for apparent facial rotation
+  Eigen::Vector3d forwardZ(0, 0, 1);
+  Eigen::Vector3d cameraTranslation(tx, ty, tz);
+  // cout << translationVector << "  " << cameraTranslation;
+  Eigen::Quaterniond cameraRot = Eigen::Quaterniond().setFromTwoVectors(forwardZ, cameraTranslation);
+  Eigen::Quaterniond newRot = cameraRot * quats;
+
+  new_marker.pose.orientation.x = newRot.vec()[0];
+  new_marker.pose.orientation.y = newRot.vec()[1];
+  new_marker.pose.orientation.z = newRot.vec()[2];
+  new_marker.pose.orientation.w = newRot.w();
 
   // Additional Visualization
   new_marker.scale.x = 1;
